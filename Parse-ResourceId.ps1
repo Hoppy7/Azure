e838056e71723821fb5a92a96bfaf6dc379c0c12
Ross Hopkins <rohopkin@microsoft.com>
2019-04-25 21:56:24 -0700

    added child resource support

--- Parse-ResourceId.ps1
+++ Parse-ResourceId.ps1
@@ -1,36 +1,54 @@
 [CmdletBinding()]
 param (
-    [Parameter(mandatory=$true)]
-    [string]$resourceId,
-
-    [Parameter(mandatory=$false)]
-    [switch]$subscriptionId,
-
-    [Parameter(mandatory=$false)]
-    [switch]$resourceGroup,
-
-    [Parameter(mandatory=$false)]
-    [switch]$resourceName
+    [Parameter(mandatory = $true)]
+    [string]$resourceId
 )
 
-$return = @{};
-
-if ($subscriptionId)
+try
 {
-    [string]$subscriptionId = [regex]::Match($resourceId, "[a-zA-Z0-9]{8}-([a-zA-Z0-9]{4}-){3}[a-zA-Z0-9]{12}").value;
-    $return.Add("subscriptionId", $subscriptionId);
+    $resourceHash = @{};
+
+    # subscription Id
+    $subscriptionId = [regex]::Match($resourceId, "[a-zA-Z0-9]{8}-([a-zA-Z0-9]{4}-){3}[a-zA-Z0-9]{12}").value;
+    $resourceHash.Add("subscriptionId", $subscriptionId);
+
+    # resource group
+    $resourceGroup = [regex]::Match($resourceId, "resourceGroups/([a-zA-Z0-9-_]+)").value.replace("resourceGroups/", "");
+    $resourceHash.Add("resourceGroup", $resourceGroup);
+
+    # resource name
+    $resourceName = [regex]::Match($resourceid, "providers/([a-zA-Z-.]+)/([a-zA-Z-.]+)/([a-zA-Z0-9-]+)").value -replace "providers/([a-zA-Z-.]+)/([a-zA-Z-.]+)/";
+    $resourceProvider = [regex]::Match($resourceid, "providers/([a-zA-Z-.]+)/([a-zA-Z-.]+)/([a-zA-Z0-9-]+)").value -replace "providers/([a-zA-Z-.]+)/";
+    $resourceProvider = $resourceProvider.Substring(0, $resourceProvider.IndexOf("/"));
+    $resourceHash.Add($resourceProvider, $resourceName);
+
+    # child resources
+    $childResources = $resourceid.Substring($resourceid.IndexOf($resourceName)) -replace "$resourceName/";
+    do 
+    {
+        $match = [regex]::Match($childResources, "/");
+
+        $childResourceProvider = [regex]::Match($childResources, "([a-zA-Z-]+)/").value;
+        $childResourceProviderValue = $childResources.Replace($childResourceProvider, "");
+        $childresources = $childResourceProviderValue.Substring($childResourceProviderValue.IndexOf("/") + 1);
+
+        if ([regex]::Match($childResourceProviderValue, "/").success -eq $true)
+        {
+            $childResourceProviderValue = $childResourceProviderValue.Substring(0, $childResourceProviderValue.IndexOf("/"));
+        }
+
+        $resourceHash.Add($childResourceProvider.replace("/", ""), $childResourceProviderValue);
+
+        if ([regex]::Match($childResources, "/").success -eq $false)
+        {
+            break;
+        }
+    }
+    while ($match.success -eq $true)
+}
+catch [exception]
+{
+    throw "Error parsing the resourceId! $($_.exception)";
 }
 
-if ($resourceGroup)
-{
-    [string]$resourceGroup = [regex]::Match($resourceId, "resourceGroups/(?=\S*['-])([a-zA-Z'-]+)").value.trim("resourceGroups/");
-    $return.Add("resourceGroup", $resourceGroup);
-}
-
-if ($resourceName)
-{
-    [string]$resourceName = $resourceId.Substring($resourceId.LastIndexOf("/") +1);
-    $return.Add("resourceName", $resourceName);
-}
-
-return $return;
+return $resourceHash;
